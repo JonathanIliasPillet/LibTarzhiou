fbb1b142b28229b59d9d260266eb0eeda2efed58
Jonathan ILIAS-PILLET <ismael@chouppy.net>
2011-04-21 21:40:31 +0200

    removed sample tests

--- tests/net/chouppy/tarzhiou/AllTests.java
+++ tests/net/chouppy/tarzhiou/AllTests.java
@@ -1,6 +1,5 @@
 package net.chouppy.tarzhiou;
 
-import net.chouppy.tarzhiou.sample_game.model.SampleGameTestSuite;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -12,7 +11,6 @@ public class AllTests
     TestSuite suite = new TestSuite("Test for net.chouppy.tarzhiou");
     //$JUnit-BEGIN$
     suite.addTest(BaseTestSuite.suite());
-    suite.addTest(SampleGameTestSuite.suite());
     //$JUnit-END$
     return suite;
   }
